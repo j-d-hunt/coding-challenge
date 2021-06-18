df49aab3ef87c412e2c6a18bf27ead8d013d3148
Jordan Hunt <72955230+j-d-hunt@users.noreply.github.com>
2021-06-17 23:03:16 -0500

    Update submission.js

--- submission.js
+++ submission.js
@@ -1,23 +1,100 @@
 const findSum = function(array) {
-  // your code here - don't forget to return a number!
+  let sum = 0; 
+  for(let i=0; i < array.length; i++){
+    sum += array[i]; 
+  }
+  return sum;
 };
 
 const findFrequency = function(array) {
-  // your code here - don't forget to return an object!
+  let elementHigh = 0;
+  let countHigh = 0;
+  let elementLow = 0;
+  let countLow = array.length;
+
+  for (let i = 0; i < array.length; i++){ //master pass
+    let tempElement = array[i];
+    let tempCount = 0;
+    for (let j = 0; j < array.length; j++){ //individual array element comparison
+      if (array[j] === tempElement)
+        tempCount++;
+    }    
+    if (tempCount > countHigh) { //finding highest count comparison
+      elementHigh = tempElement;
+      countHigh = tempCount;
+    }
+    if (tempCount < countLow) { //finding lowest count comparison
+      elementLow = tempElement;
+      countLow = tempCount;
+    }
+  }
+  return console.log("most: '" + elementHigh + "' least: '" + elementLow + "'");
 };
 
 const isPalindrome = function(str) {
-  // your code here - don't forget to return a boolean!
+  const length = str.length;
+  for (let i = 0; i < length / 2; i++) {
+    if (str.charAt(i) !== str.charAt(length - 1 -i)) {
+      return false
+    }
+  }
+  return true;
 };
 
 const largestPair = function(array) {
-  // your code here - don't forget to return a number!
+  const largestPair = function(array) {
+  let arr = array;
+  let currentElement = 0;
+  let nextElement = 0;
+  let product = -Infinity;
+  for (let i = 0; i - 1 < arr.length; i++) {
+    currentElement = arr[i];
+    nextElement = arr[i + 1];
+    if (currentElement * nextElement > product) {
+      product = currentElement * nextElement;
+    };
+  };
+  return product;
 };
 
 const removeParenth = function(str) {
-  // your code here - don't forget to return a string!
+  str = str.replace(/ *\([^)]*\) */g, "");
+  return str
 };
 
 const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
+  const letterValues = {
+    a: 1,
+    e: 1,
+    i: 1,
+    o: 1,
+    u: 1,
+    l: 1,
+    n: 1,
+    r: 1,
+    s: 1,
+    t: 1,
+    d: 2,
+    g: 2,
+    b: 3,
+    c: 3,
+    m: 3,
+    p: 3,
+    f: 4,
+    h: 4,
+    v: 4,
+    w: 4,
+    y: 4,
+    k: 5,
+    j: 8,
+    x: 8,
+    q: 10,
+    z: 10,
+  };
+    let yourScore = 0;
+    str = str.toLowerCase();
+    for (let ch of str) {
+        yourScore += letterValues[ch];
+    }
+    return yourScore;
 };
